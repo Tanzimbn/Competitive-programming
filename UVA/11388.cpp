dd3cb3fd5f0a4ebe331361197baef55f0cf8947f
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2021-04-09 19:23:20 +0600

    Create 11388.cpp

---
+++ UVA/11388.cpp
@@ -0,0 +1,120 @@
+//...Bismillahir Rahmanir Rahim. . .
+// Team CUET_DosWings
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// typedefs...
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+// constants...
+const double PI = acos(-1);
+const ll mod = 1e9+7; // 998244353;
+const int MXS = 2e5+5;
+const ll MXI = 1e9+5;
+const ll MXL = 1e18+5;
+const ll INF = 1e9+5;
+const ll INFLL = 1e18+5;
+const ll EPS = 1e-9;
+
+// defines...
+#define MP        make_pair
+#define PB        push_back
+#define fi         first
+#define se         second
+#define sz(x)      (int)x.size()
+#define all(x)      begin(x), end(x)
+#define si(a)       scanf("%d", &a)
+#define sii(a, b)    scanf("%d%d", &a, &b)
+#define ordered_set tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>
+#define boost_      ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define iter_(i,n)    for (int i = 0; i < int(n); i++)
+#define for_n(i, n)  for (int i = 1; i <= int(n); i++)
+#define print_array(a) for(int i=0;i<n;i++)  cout<<a[i]<<" ";
+#define rev(i,n)     for(int i=n;i>=0;i--)
+#define itr          ::iterator
+#define s_sort(s)    sort(s.begin(),s.end())
+#define n_sort(a)    sort(a,a+n)
+#define precise_impact cout<<setprecision(10)<<fixed;
+#define endl         "\n"
+
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const{ auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2;}
+};
+
+// functions...
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+ll bigmod(ll a,ll b){ if(b==0){ return 1;} ll tm=bigmod(a,b/2); tm=(tm*tm)%mod; if(b%2==1) tm=(tm*a)%mod; return tm;}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+int a[100005];
+vector<int> p;
+void seive()
+{
+	p.PB(2);
+	for(int i=3;i*i<=100000;i=i+2)
+	{
+		if(a[i]==0)
+		{
+            for(int j=i*i;j<=100000;j=j+2*i)
+            a[j]=1;
+		}
+	}
+	for(int i=3;i<=100000;i=i+2)
+	{
+		if(a[i]==0) p.PB(i);
+	}
+}
+int main()
+{
+   seive();
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+      ll g,l,gg,ll,check=0;
+      cin>>g>>l;
+      gg=g,ll=l;
+      for(int i=0;i<p.size();i++)
+      {
+      	 int temp=0,temp1=0;
+      	 while(gg%p[i]==0)
+      	 {
+      	 	temp++; gg/=p[i];
+      	 }
+      	 while(ll%p[i]==0)
+      	 {
+      	 	temp1++; ll/=p[i];
+      	 }
+      	 if(temp>temp1)
+      	 	{check=1;break;}
+      }
+      if(gg!=1&&gg!=ll)
+      check=1;
+      if(check) cout<<-1<<endl;
+      else cout<<g<<" "<<l<<endl;
+   }
+}
