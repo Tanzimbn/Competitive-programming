9b7a76cc1d64ffcb6500f62ad175df4f84f1b74a
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2021-04-22 16:46:04 +0600

    Create 11327.cpp

---
+++ UVA/11327.cpp
@@ -0,0 +1,97 @@
+//...Bismillahir Rahmanir Rahim. . .
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// typedefs...
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+// constants...
+const double PI = acos(-1);
+const ll mod = 1e9+7; // 998244353;
+const int MXS = 2e5+5;
+const ll MXI = 1e9+5;
+const ll MXL = 1e18+5;
+const ll INF = 1e9+5;
+const ll INFLL = 1e18+5;
+const ll EPS = 1e-9;
+
+// defines...
+#define MP        make_pair
+#define PB        push_back
+#define fi         first
+#define se         second
+#define sz(x)      (int)x.size()
+#define all(x)      begin(x), end(x)
+#define si(a)       scanf("%d", &a)
+#define sii(a, b)    scanf("%d%d", &a, &b)
+#define ordered_set tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>
+#define boost_      ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define iter_(i,n)    for (int i = 0; i < int(n); i++)
+#define for_n(i, n)  for (int i = 1; i <= int(n); i++)
+#define print_array(a) for(int i=0;i<n;i++)  cout<<a[i]<<" ";
+#define rev(i,n)     for(int i=n;i>=0;i--)
+#define itr          ::iterator
+#define s_sort(s)    sort(s.begin(),s.end())
+#define n_sort(a)    sort(a,a+n)
+#define precise_impact cout<<setprecision(10)<<fixed;
+#define endl         "\n"
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+ll bigmod(ll a,ll b){ if(b==0){ return 1;} ll tm=bigmod(a,b/2); tm=(tm*tm)%mod; if(b%2==1) tm=(tm*a)%mod; return tm;}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+ll phi[200005];
+int main()
+{
+    phi[0]=0;
+    phi[1]=2;
+    for(int i=2;i<=200000;i++) phi[i]=i;
+    for(int i=2;i<=200000;i++)
+    {
+        if(phi[i]==i)
+        {
+            for(int j=i;j<=200000;j+=i)
+            {
+                phi[j]/=i;
+                phi[j]*=(i-1);
+            }
+        }
+    }
+    for(int i=2;i<=200000;i++) phi[i]+=phi[i-1];
+    ll n;
+    while(cin>>n)
+    {
+        if(n==0) return 0;
+
+        ll l=0,h=200000,ans=0;
+        while(l<h)
+        {
+            ll mid=(l+h)/2;
+            if(phi[mid]>=n) h=mid;
+            else  l=mid+1;
+        }
+        int temp=0;
+        for(int i=0;i<=phi[l];i++)
+        {
+            if(gcd(i,l)==1) ++temp;
+            if(n-phi[l-1]==temp)
+            {
+                cout<<i<<"/"<<l<<endl;
+                break;
+            }
+        }
+    }
+}
