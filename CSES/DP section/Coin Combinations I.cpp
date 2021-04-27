48b81cdd76928cac159e34ec48abde590435c2c1
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2021-04-27 16:50:54 +0600

    Create Coin Combinations I.cpp

---
+++ CSES/DP section/Coin Combinations I.cpp
@@ -0,0 +1,43 @@
+//Bismillahir Rahmanir Raheem
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+#define fi first
+#define se second
+#define PI 3.14159265
+#define mod  1000000007
+#define ordered_set tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>
+///order_of_key find_by_order
+const ll INFLL = 1e18;
+const ll INF =1e9;
+const ll EPS =1e-9;
+void fast()
+{
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+}
+ll gcd(ll a,ll b){if(b==0) return a; else return gcd(b,a%b);}
+ll lcm(ll a,ll b){ return (a*b)/gcd(a,b);}
+ll ncr(ll n,ll r){ ll ans=1;for(ll i=1;i<=r;i++) ans=(ans*(n-i+1))/i;return ans;}
+ll a[105],num[1000005];
+int main()
+{
+    ll n,x;
+    cin>>n>>x;
+    for(int i=0;i<n;i++) cin>>a[i];
+    num[0]=1;
+    for(int j=0;j<=x;j++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(j-a[i]>=0&&num[j-a[i]])
+            num[j]=(num[j]+num[j-a[i]])%mod;//cout<<num[j]<<endl;
+        }
+    }
+    cout<<num[x]<<endl;
+}
