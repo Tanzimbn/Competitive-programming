26dd5674f25ba681d5b86089d8303a7f83f757a0
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2021-04-27 16:49:02 +0600

    Create Grid Paths.cpp

---
+++ CSES/DP section/Grid Paths.cpp
@@ -0,0 +1,48 @@
+//Bismillahir Rahmanir Raheem
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+#define fi first
+#define se second
+#define PI 3.14159265
+#define mod  1000000007
+#define ordered_set tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>
+///order_of_key find_by_order
+const ll INFLL = 1e18;
+const ll INF =1e9;
+const ll EPS =1e-9;
+void fast()
+{
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+}
+ll gcd(ll a,ll b){if(b==0) return a; else return gcd(b,a%b);}
+ll lcm(ll a,ll b){ return (a*b)/gcd(a,b);}
+ll ncr(ll n,ll r){ ll ans=1;for(ll i=1;i<=r;i++) ans=(ans*(n-i+1))/i;return ans;}
+ll n,dp[1006][1006];
+string s[1006];
+ll check(int x,int y)
+{
+    if(x==n&&y==n) return 1;
+    if(dp[x][y]!=-1) return dp[x][y];
+    ll ans=0;
+    if(x+1<=n&&s[x][y-1]!='*') ans=(ans+check(x+1,y))%mod;
+    if(y+1<=n&&s[x-1][y]!='*') ans=(ans+check(x,y+1))%mod;
+    return dp[x][y]=ans;
+}
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++) cin>>s[i];
+    for(int i=0;i<=n;i++)
+    for(int j=0;j<=n;j++)
+    dp[i][j]=-1;
+    if(s[0][0]!='*')
+    cout<<check(1,1)<<endl;
+    else cout<<0<<endl;
+}
