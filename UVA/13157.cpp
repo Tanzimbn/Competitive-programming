c5cb6d6ee6f3b0533caa4cc0a445264d055e3f90
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2022-01-19 17:58:43 +0600

    Create 13157.cpp

---
+++ UVA/13157.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// typedefs...
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update>pref_trie;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+// constants...
+const double PI = acos(-1);
+const ll mod =1000000007 ; // 998244353
+const int MXS = 2e5+5;
+const ll MXI = 1e9+5;
+const ll MXL = 1e18+5;
+const ll INF = 1e9+5;
+const ll INFLL = 1e18+5;
+const ll EPS = 1e-9;
+
+// defines...
+#define MP        make_pair
+#define PB        push_back
+#define fi         first
+#define se         second
+#define sz(x)      (int)x.size()
+#define all(x)      begin(x), end(x)
+#define si(a)       scanf("%d", &a)
+#define sii(a, b)    scanf("%d%d", &a, &b)
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+#define boost_      ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define iter_(i,n)    for (int i = 0; i < int(n); i++)
+#define for_n(i, n)  for (int i = 1; i <= int(n); i++)
+#define print_array(a) for(int i=0;i<n;i++)  cout<<a[i]<<" ";
+#define rev(i,n)     for(int i=n;i>=0;i--)
+#define itr          ::iterator
+#define s_sort(s)    sort(s.begin(),s.end())
+#define n_sort(a, n)    sort(a,a+n)
+#define precise_impact cout<<setprecision(10)<<fixed;
+#define endl         "\n"
+// functions...
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+ll bigmod(ll a,ll b){ if(b==0){ return 1;} ll tm=bigmod(a,b/2); tm=(tm*tm)%mod; if(b%2==1) tm=(tm*a)%mod; return tm;}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+ll modpow(ll a,ll p) {ll ans=1;while(p){if(p%2)ans=(ans*a)%mod;a=(a*a)%mod;p/=2;} return ans;}
+ll inverse_mod(ll n) {return modpow(n,mod-2);}
+//ll fact[200005];
+//ll ncr_mod(ll n,ll r) {return (((fact[n]*inverse_mod(fact[r]))%mod)*inverse_mod(fact[n-r]))%mod;}
+vector<ll> prime;
+void solve()
+{
+   ll n,m;
+   cin>>n>>m;
+   ll ans=n;
+   for(int i=0;i<prime.size()&&prime[i]+1<=n;i++)
+   {
+      ans=(ans+(n-prime[i])*(m-1))%mod;
+   }
+   for(int i=1;i<prime.size()&&prime[i]+3<=n;i++)
+   {
+     if(prime[i]+2!=prime[i+1]) continue;
+     ll temp=(((m-1)*(m-2))/2)%mod;
+     ans=(ans+2*((n-prime[i]-2)*temp)%mod)%mod;
+   }
+   for(int i=1;i<prime.size()&&prime[i]+5<=n;i++)
+   {
+     if(prime[i]+2!=prime[i+1]||prime[i]+4!=prime[i+2]) continue;
+     ll temp=(((m-1)*(m-2)*(m-3))/6)%mod;
+     ans=(ans+((n-prime[i]-4)*temp)%mod)%mod;
+   }
+   cout<<ans<<endl;
+} 
+int main()
+{
+    for(int i=2;i<=100005;i++)
+    {
+      int temp=1;
+      for(int j=2;j*j<=i;j++)
+      {
+        if(i%j==0) temp=0;
+      }
+      if(temp) prime.PB(i);
+    }
+    int t=1;
+    cin>>t; 
+    for(int i=1;i<=t;i++)
+    {
+       cout<<"Case "<<i<<": ";
+       solve();
+    }
+}
+// *
+// *prime*
+// *2*prime*
+// *2*prime*2*
