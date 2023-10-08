dc01e392f4e6c0b1012694dd7d92ea9f1cb5875c
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2023-10-08 18:56:36 +0600

    Update Centroid Decomposition.cpp

--- Template/Centroid Decomposition.cpp
+++ Template/Centroid Decomposition.cpp
@@ -1,31 +1,33 @@
-const int N= 1e5+5; // check
-int sub[N],par[N];
-set<int> Tree[N];
-int get_sub(int u,int p){
-
+const int N= 2e5+5; // check
+int sub[N], par[N], lvl[N], vis[N], cnt[N];
+vector<int> Tree[N];
+int get_sub(int u,int p) {
     sub[u]=1;
     for(auto v : Tree[u]){
-        if(v!=p) sub[u]+=get_sub(v,u);
+        if(v != p && vis[v] == 0) sub[u]+=get_sub(v, u);
     }
     return sub[u];
 }
-int get_centroid(int u,int p,int n){
-
+int get_centroid(int u,int p,int n) {
     for(auto v:Tree[u]){
-        if(v!=p and sub[v]>n/2)
-        return get_centroid(v,u,n);
+        if(vis[v]) continue;
+        if(v != p && sub[v] > n/2)
+        return get_centroid(v, u, n);
     }
     return u;
 }
-void build_centroid(int u,int p = -1){ // p = -1 for root 
-    int n=get_sub(u,p);
-    int centroid=get_centroid(u,p,n);
-    if(p==-1) p=centroid;
-    par[centroid]=p;
-    // for each Tree resulting from the removal of the centroid
-    for(auto v:Tree[centroid]){
-        Tree[v].erase(centroid);
-        build_centroid(v,centroid);
+void add_centroid(int x, int y) {
+    par[y] = x;
+    lvl[y] = lvl[x] + 1;
+    vis[y] = 1;
+}
+void build_centroid(int u,int p = -1) {
+    int n=get_sub(u,p); // subtree size
+    int centroid = get_centroid(u,p,n);
+    if(p == -1) p = centroid;
+    add_centroid(p, centroid);
+    for(auto v : Tree[centroid]){
+        if(vis[v]) continue;
+        build_centroid(v, centroid);
     }
-    return;
 }
