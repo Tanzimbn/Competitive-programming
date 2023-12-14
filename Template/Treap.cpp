6d6421cd8cd8c48319edebb0ba44e6529ea29a08
Tanzim Bin Nasir <61573605+Tanzimbn@users.noreply.github.com>
2023-12-14 21:46:12 +0600

    Update Treap.cpp

--- Template/Treap.cpp
+++ Template/Treap.cpp
@@ -1,86 +1,87 @@
-struct Treap {
-    struct Node {
-        int x = 0;
-        int pr;
-        char c;
-        int sz = 1;
-        Node* left = nullptr;
-        Node* right = nullptr;
- 
-        Node(char c): c(c) {
-            pr = rand();
-        }
-    };
- 
-    Node* root = nullptr;
- 
-    Treap() {}
- 
-    void push_back(char c) {
-        root = merge(root, new Node(c));
-    }
- 
-    void push(Node* t) {
-        if (t && t->x) {
-            t->x = 0;
-            swap(t->left, t->right);
-            if (t->left) t->left->x ^= 1;
-            if (t->right) t->right->x ^= 1;
-        }
-    }
- 
- 
-    void split(Node* t, Node* &left, Node* &right, int x) {
-        if (!t) {
-            left = right = nullptr;
-            return;
-        }
-        int left_size = (t->left ? t->left->sz : 0);
-        if (x <= left_size) {
-            split(t->left, left, t->left, x);
-            right = t; update(right);
-        }
-        else {
-            split(t->right, t->right, right, x-left_size-1);
-            left = t; update(left);
-        }
-    }
- 
-    Node* merge(Node* left, Node* right) {
-        if (!left) return right;
-        if (!right) return left;
-        if (left->pr < right->pr) {
-            left->right = merge(left->right, right);
-            update(left);
-            return left;
-        }
-        else {
-            right->left = merge(left, right->left);
-            update(right);
-            return right;
-        }
-    }
- 
-    int size(Node* &v) { return v ? v->sz : 0; }
- 
-    void update(Node* &v) { v->sz = 1 + size(v->left) + size(v->right); }
- 
-    void func(int l, int r) {
-        Node* first = nullptr;
-        Node* mid = nullptr;
-        Node* last = nullptr;
-        split(root, first, last, l);
-        split(last, mid, last, r - (first ? first->sz : 0) + 1);
-        root = merge(merge(first, last), mid);
-    }
- 
-    void ans(Node* t) {
-        if (!t) return;
-        ans(t->left);
-        cout << t->c;
-        ans(t->right);
-    }
-    void help() {
-        ans(root);
+using treap_val = char; // treap value data type
+struct node {
+    node *L, *R;
+    int W, S;
+    treap_val V;
+    bool F;
+    node(char x) {
+        L = R = 0;
+        W = rand();
+        S = 1;
+        V = x;
+        F = 0;
     }
 };
+
+int size(node *treap) {
+    return (treap == 0 ? 0 : treap->S);
+}
+
+void push(node *treap) {
+    if (treap && treap->F) {
+        treap->F = 0;
+        swap(treap->L, treap->R);
+        if (treap->L) treap->L->F ^= 1;
+        if (treap->R) treap->R->F ^= 1;
+    }
+}
+// k -> 1-indexed || K index in left tree
+void split(node *treap, node *&left, node *&right, int k) {
+    if (treap == 0)
+        left = right = 0;
+    else {
+        push(treap);
+        if (size(treap->L) < k) {
+            split(treap->R, treap->R, right, k - size(treap->L) - 1);
+            left = treap;
+        }
+        else {
+            split(treap->L, left, treap->L, k);
+            right = treap;
+        }
+        treap->S = size(treap->L) + size(treap->R) + 1;
+    }
+}
+// root - left root - right root -> merge tree root is treap
+void merge(node *&treap, node *left, node *right) {
+    if (left == 0) treap = right;
+    else if (right == 0) treap = left;
+    else {
+        push(left);
+        push(right);
+        if (left->W < right->W) {
+            merge(left->R, left->R, right);
+            treap = left;
+        }
+        else {
+            merge(right->L, left, right->L);
+            treap = right;
+        }
+        treap->S = size(treap->L) + size(treap->R) + 1;
+    }
+}
+void add(node *&treap, treap_val x) {
+    merge(treap, treap, new node(x));
+}
+// pos -> 1-indexed 
+void remove(node *&treap, int pos) {
+    node *A, *B, *C;
+    split(treap, A, B, pos - 1);
+    split(B, B, C, 1);
+    merge(treap, A, C);
+}
+void reverse(node *&treap, int x, int y) {
+    node *A, *B, *C;
+    split(treap, A, B, x - 1);
+    split(B, B, C, y - x + 1);
+    B->F ^= 1;
+    merge(treap, A, B);
+    merge(treap, treap, C);
+}
+void print(node *treap) {
+    if (treap == NULL) return;
+    push(treap);
+    print(treap->L);
+    cout << treap->V;
+    print(treap->R);
+}
